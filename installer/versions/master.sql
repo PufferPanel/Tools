6a79f04113d480c0af4d15a874bc4ed6f9b0fb13
Lord_Ralex <lordralex@gmail.com>
2015-04-11 21:18:42 -0500

    Continue database updates - If sub users break, blame @DaneEveritt

--- installer/versions/master.sql
+++ installer/versions/master.sql
@@ -1,13 +1,15 @@
+-- Remove existing database and create new
 DROP DATABASE IF EXISTS `pufferpanel`;
 CREATE DATABASE `pufferpanel` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci;
 USE `pufferpanel`;
 
+-- Create the tables needed
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `whmcs_id` int(10) unsigned DEFAULT NULL,
   `uuid` char(36) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `email` text NOT NULL,
+  `email` varchar(256) NOT NULL,
   `password` text NOT NULL,
   `language` char(2) NOT NULL DEFAULT 'en',
   `register_time` int(15) unsigned NOT NULL,
@@ -17,9 +19,10 @@ CREATE TABLE `users` (
   `notify_login_s` tinyint(1) unsigned NOT NULL DEFAULT '1',
   `notify_login_f` tinyint(1) unsigned NOT NULL DEFAULT '1',
   `use_totp` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `totp_secret` text,
+  `totp_secret` varchar(16),
   PRIMARY KEY (`id`),
   UNIQUE KEY `uuid_unique` (`uuid`),
+  UNIQUE KEY `email_unique` (`email`),
   KEY `uuid_key` (`uuid`)
 ) ENGINE=InnoDB;
 
@@ -41,8 +44,7 @@ CREATE TABLE `acp_settings` (
   `setting_ref` char(25) NOT NULL,
   `setting_val` text,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `setting_ref_unique` (`setting_ref`),
-  KEY `setting_ref_key` (`setting_ref`)
+  UNIQUE KEY `setting_ref_unique` (`setting_ref`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE `actions_log` (
@@ -72,7 +74,8 @@ CREATE TABLE `locations` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `short` varchar(10) NOT NULL,
   `long` varchar(500) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `short_unique` (`short`)
 ) ENGINE=InnoDB;
 
 CREATE TABLE `nodes` (
@@ -105,14 +108,8 @@ CREATE TABLE `plugins` (
   `name` varchar(100) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `slug_unique` (`slug`),
-  KEY `slug_key` (`slug`)
+  UNIQUE KEY `slug_unique` (`slug`)
 ) ENGINE=InnoDB;
-
-INSERT INTO `plugins` (`id`, `hash`, `slug`, `name`, `description`) VALUES
-	(1, '37d8949d-5da2-4390-a28f-27ac1babc4da', 'minecraft', 'Minecraft', 'Minecraft is a game about breaking and placing blocks. At first, people built structures to protect against nocturnal monsters, but as the game grew players worked together to create wonderful, imaginative things. This version of the plugin is ment for versions of the game <strong>greater than 1.7.0</strong>.'),
-	(2, 'b4b90feb-6adb-499c-a9f8-09b6e80c9d16', 'minecraft-pre', 'Minecraft (pre 1.7)', 'Minecraft is a game about breaking and placing blocks. At first, people built structures to protect against nocturnal monsters, but as the game grew players worked together to create wonderful, imaginative things. This version of the plugin is ment for versions of the game <strong>less than 1.7.0</strong>.'),
-	(3, '64a8fe48-0b69-4e8a-96c4-14c60309c6c1', 'bungeecord', 'BungeeCord', 'For a long time, Minecraft server owners have had a dream that encompasses a free, easy, and reliable way to connect multiple Minecraft servers together. BungeeCord is the answer to said dream. Whether you are a small server wishing to string multiple game-modes together, or the owner of the ShotBow Network, BungeeCord is the ideal solution for you. With the help of BungeeCord, you will be able to unlock your community\'s full potential.');
 
 CREATE TABLE `servers` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -156,14 +153,21 @@ CREATE TABLE `permissions` (
 
 CREATE TABLE `subusers` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `uuid` char(36) NOT NULL,
   `user` int(10) unsigned NOT NULL,
   `server` int(10) unsigned NOT NULL,
   `daemon_secret` char(36) NOT NULL,
   `daemon_permissions` mediumtext,
   `permissions` mediumtext,
   `pending` tinyint(1) unsigned NOT NULL,
-  `pending_email` varchar(200) DEFAULT NULL,
+  `pending_email` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uuid` (`uuid`)
+  UNIQUE KEY `uuid` (`uuid`),
+  CONSTRAINT `FK_subusers_user` FOREIGN KEY (`user`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_subusers_server` FOREIGN KEY (`server`) REFERENCES `server` (`id`)
 ) ENGINE=InnoDB;
+
+-- Insert all default data
+INSERT INTO `plugins` (`id`, `hash`, `slug`, `name`, `description`) VALUES
+	(1, '37d8949d-5da2-4390-a28f-27ac1babc4da', 'minecraft', 'Minecraft', 'Minecraft is a game about breaking and placing blocks. At first, people built structures to protect against nocturnal monsters, but as the game grew players worked together to create wonderful, imaginative things. This version of the plugin is ment for versions of the game <strong>greater than 1.7.0</strong>.'),
+	(2, 'b4b90feb-6adb-499c-a9f8-09b6e80c9d16', 'minecraft-pre', 'Minecraft (pre 1.7)', 'Minecraft is a game about breaking and placing blocks. At first, people built structures to protect against nocturnal monsters, but as the game grew players worked together to create wonderful, imaginative things. This version of the plugin is ment for versions of the game <strong>less than 1.7.0</strong>.'),
+	(3, '64a8fe48-0b69-4e8a-96c4-14c60309c6c1', 'bungeecord', 'BungeeCord', 'For a long time, Minecraft server owners have had a dream that encompasses a free, easy, and reliable way to connect multiple Minecraft servers together. BungeeCord is the answer to said dream. Whether you are a small server wishing to string multiple game-modes together, or the owner of the ShotBow Network, BungeeCord is the ideal solution for you. With the help of BungeeCord, you will be able to unlock your community's full potential.');
